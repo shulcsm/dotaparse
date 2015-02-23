bc33e81fc08c3ab8ca3e477207809d162035f164
Mārtiņš Šulcs <shulcsm@gmail.com>
2015-02-23 23:21:20 +0200

    Validate header.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@ extern crate "rustc-serialize" as rustc_serialize;
 extern crate docopt;
 
 use docopt::Docopt;
-
 use std::path::Path;
 use std::io::{Read, ReadExt, BufReader, BufStream};
 use std::fs::File;
 
 
-// Write the Docopt usage string.
 static USAGE: &'static str = "
 Usage: dotaparse <file>
 ";
@@ -24,12 +22,14 @@ fn main() {
                             .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
 
-
 	let mut f = File::open(&args.arg_file).unwrap();
 	let mut stream = BufStream::new(f);
 	let mut reader = BufReader::new(stream);
-	let mut string = String::new();
-	reader.take(8).read_to_string(&mut string);
-	println!("{:?}", string);
+
+	let mut header = String::new();
+	reader.take(8).read_to_string(&mut header);
+	assert!("PBUFDEM\0" == header, "Doesn't look like a protobuf file.");
+
+	println!("{:?}", header);
     println!("Hello, world!")
 }
