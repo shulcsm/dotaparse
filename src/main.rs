4f8283956fafbb0f7980b0eb1e7c34794f785564
Mārtiņš Šulcs <shulcsm@gmail.com>
2015-04-24 13:33:07 +0300

    Read gameinfo offset.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate rustc_serialize;
 extern crate docopt;
+extern crate byteorder;
 
 use docopt::Docopt;
-use std::path::Path;
-use std::io::{Read, BufReader, BufStream};
+use std::io::{Read, Seek, SeekFrom};
 use std::fs::File;
+use byteorder::{ReadBytesExt, LittleEndian};
 
+use proto::demo;
 
 static USAGE: &'static str = "
 Usage: dotaparse <file>
@@ -22,13 +24,18 @@ fn main() {
                             .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
 
-	let f = File::open(&args.arg_file).unwrap();
-	let stream = BufStream::new(f);
-	let reader = BufReader::new(stream);
+	let mut reader = File::open(&args.arg_file).unwrap();
 
-	let mut header = String::new();
-	reader.take(8).read_to_string(&mut header);
+  // This seems wrong
+	let mut header_buf = [0; 8];
+  reader.read(&mut header_buf);
+  let header = String::from_utf8_lossy(&mut header_buf);
+
+  let game_info_offset = reader.read_u32::<LittleEndian>().unwrap();
+  reader.seek(SeekFrom::Start(game_info_offset as u64));
+
+  println!("{:?}", header);
 	assert!("PBUFDEM\0" == header, "Doesn't look like a scrds demo file.");
+  println!("{:?}", game_info_offset);
 
-	println!("{:?}", header);
 }
