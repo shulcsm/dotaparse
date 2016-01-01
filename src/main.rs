da99fdcfdbc6217f934ef3d656662c7abf5e0097
Mārtiņš Šulcs <shulcsm@gmail.com>
2016-01-01 15:45:16 +0200

    Compiles, update deps.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::{Read, Seek, SeekFrom};
 use std::fs::File;
 use byteorder::{ReadBytesExt, LittleEndian};
 
-use proto::demo;
+mod proto;
 
 static USAGE: &'static str = "
 Usage: dotaparse <file>
@@ -20,22 +20,23 @@ struct Args {
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
-                            .unwrap_or_else(|e| e.exit());
+                        .and_then(|d| d.decode())
+                        .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
 
-	let mut reader = File::open(&args.arg_file).unwrap();
+    let mut reader = File::open(&args.arg_file).unwrap();
 
-  // This seems wrong
-	let mut header_buf = [0; 8];
-  reader.read(&mut header_buf);
-  let header = String::from_utf8_lossy(&mut header_buf);
+    // This seems wrong
+    let mut header_buf = [0; 8];
+    reader.read(&mut header_buf);
+    let header = String::from_utf8_lossy(&mut header_buf);
 
-  let game_info_offset = reader.read_u32::<LittleEndian>().unwrap();
-  reader.seek(SeekFrom::Start(game_info_offset as u64));
+    let game_info_offset = reader.read_u32::<LittleEndian>().unwrap();
+    reader.seek(SeekFrom::Start(game_info_offset as u64));
 
-  println!("{:?}", header);
-	assert!("PBUFDEM\0" == header, "Doesn't look like a scrds demo file.");
-  println!("{:?}", game_info_offset);
+    println!("{:?}", header);
+    // Source1: PBUFDEM, Source2: PBDEMS2
+    assert!("PBDEMS2\0" == header, "Doesn't look like a scrds demo file.");
+    println!("{:?}", game_info_offset);
 
 }
